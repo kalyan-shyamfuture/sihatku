9b2aa9c2ffc8cac5153e7928fd76ad9a7f957a38
Kalyan Acharya <tonmoy.1984@gmail.com>
2020-01-04 11:31:04 +0530

    new merge code image slider

--- src/app/layout/order/my-procedures/my-procedures.component.ts
+++ src/app/layout/order/my-procedures/my-procedures.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-my-procedures',
   templateUrl: './my-procedures.component.html',
   styleUrls: ['./my-procedures.component.scss']
 })
 export class MyProceduresComponent implements OnInit {
+  bannerOptions: OwlOptions = {
+    loop: true,
+    autoplay: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
 
-  constructor() { }
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: false
+  }
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit() {
+  }
+  modalRef: BsModalRef;
+  config = {
+    animated: true
+  };
+
+  images: any = [];
+  mediaList: any = [
+    {
+      image: '../../../../assets/img/test.jpg',
+    },
+    {
+      image: '../../../../assets/img/test1.jpg',
+    },{
+      image: '../../../../assets/img/test2.jpg',
+    },
+  ];
+ 
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, this.config);
   }
 
 }
--- src/app/layout/order/practioners/practioners.component.ts
+++ src/app/layout/order/practioners/practioners.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,TemplateRef } from '@angular/core';
 import {  MainService} from "../../../core/services/main.service";
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-practioners',
   templateUrl: './practioners.component.html',
@@ -7,8 +9,51 @@ import {  MainService} from "../../../core/services/main.service";
 })
 export class PractionersComponent implements OnInit {
   userId:any;
+  modalRef: BsModalRef;
+  config = {
+    animated: true
+  };
+
+  bannerOptions: OwlOptions = {
+    loop: true,
+    autoplay: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: false
+  }
+
+  mediaList: any = [
+    {
+      image: '../../../../assets/img/test.jpg',
+    },
+    {
+      image: '../../../../assets/img/test1.jpg',
+    },{
+      image: '../../../../assets/img/test2.jpg',
+    },
+  ];
   constructor(
     private mainService: MainService,
+    private modalService: BsModalService
   ) { 
     this.userId = localStorage.getItem('userId');
   }
@@ -26,6 +71,10 @@ export class PractionersComponent implements OnInit {
           console.log(error.error); 
         }
     )
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, this.config);
   }
 
 }
